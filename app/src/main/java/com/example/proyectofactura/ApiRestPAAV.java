69a9b36d8fa36a19d805436269a47e7b967bdb39
Pablo <alejozx456@gmail.com>
2022-02-09 20:15:56 -0500

    20h15

--- app/src/main/java/com/example/proyectofactura/ApiRestPAAV.java
+++ app/src/main/java/com/example/proyectofactura/ApiRestPAAV.java
@@ -6,7 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
-public interface ApiRest {
+public interface ApiRestPAAV {
     @GET("productos")
     Call<List<ProductoPAAV>> obtenerProductos();
 
